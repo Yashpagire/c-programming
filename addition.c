6a1236b1472006307a467a23611bc475c293c593
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-08-16 09:12:02 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,15 @@
+// write a program for addition of two number
+#include<stdio.h>
+
+int main()
+{
+  
+	int num;
+	num = 50 + 50;
+
+	printf("\naddition of %d+%d=%d", 50, 50, num);
+ 
+ 
+ return(0);
+
+}
