adf1d7c67a17c7a2f7b98731360a00202fee7d54
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-08-22 15:03:45 +0530

    Default value

---
+++ df.c
@@ -0,0 +1,16 @@
+//print all default value
+#include<stdio.h>
+int main()
+{
+	int a;
+	char ch;
+	float f;
+	double d;
+
+	printf("\nvalue of a=%d", a);
+	printf("\nvalue of ch=%d", ch);
+	printf("\nvalue of f=%d", f);
+	printf("\nvalue of d=%d", d);
+
+	return(0);
+}
