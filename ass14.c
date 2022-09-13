00c549119aa105d77d45b700c2ab9fcf6ada5a58
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-09-13 21:18:59 +0530

    Create ass14.c

---
+++ ass14.c
@@ -0,0 +1,18 @@
+//declare array and assign 5 float values and print all values
+#include<stdio.h>
+int main()
+{
+	float arr[4];
+
+	arr[0] = 12.1f;
+	arr[1] = 12.2f;
+	arr[2] = 12.3f;
+	arr[3] = 12.4f;
+
+	printf("\narr[0]=%.1f", arr[0]);
+	printf("\narr[1]=%.1f", arr[1]);
+	printf("\narr[2]=%.1f", arr[2]);
+	printf("\narr[3]=%.1f", arr[3]);
+
+	return(0);
+}
