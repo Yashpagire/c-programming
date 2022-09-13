3bd216182d1892636b574c084d1c7f10f5b457d1
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-09-13 21:09:27 +0530

    Create ass6.c

---
+++ ass6.c
@@ -0,0 +1,23 @@
+/*print bellow pattern  *
+                        **
+                        ***
+                        ****
+                        ***** */
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+            if (i >= j)
+            {
+                printf("*");
+            }
+           
+        printf("\n");
+    }
+    
+    return(0);
+}
