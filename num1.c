69a4bb5efc5125b30a757de91a21cb1f1dc4fb9b
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-09-13 20:44:45 +0530

    Create num1.c

---
+++ num1.c
@@ -0,0 +1,24 @@
+/*print bellow pattern     A
+						               B C
+						               D E F
+						               G H I J
+						               K L M N O  */
+#include<stdio.h>
+int main()
+{
+	char i, j, count = 'A';
+
+
+	for (i = 'A'; i <= 'E'; i++)
+	{
+		for (j = 'A'; j <= i; j++)
+		{
+			printf("%c ", count);
+			count++;
+		}
+		printf("\n");
+
+
+	}
+	return(0);
+}
