ed9dd941714e29005af7b9d1b7e16ce48d584795
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-09-13 16:37:13 +0530

    switch case

--- array.c
+++ array.c
@@ -1,10 +1,33 @@
-//find sum 
+// find sum,mul,sub,dev using switch case
 #include<stdio.h>
-int main(int argc, char* argv[])
+ int main(int argc, char *argv[])
 {
+	//local variable declaration
+	int a, b;
+	float ans = 0.0f;
+	char symbol;
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	symbol = argv[2][0];
 
+	switch (symbol)
+	{
+	case'+':
+		ans = a + b; break;
 
-	printf("%d", (atoi(argv[1]) + atoi(argv[2]));
+	case'-':
+		ans = a - b; break;
+
+	case'*':
+		ans = a * b; break;
+  
+	case'/':
+		ans = (float)a / (float)b; break;
+
+	default:
+		printf("\n invalid operation");
+		exit(1);
+    }
+	printf("\n ans=%.1f", ans);
 	return(0);
-
 }
