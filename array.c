7613ada1e590800bdbc362851699bf5fcb2151f6
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-09-13 16:34:02 +0530

    Add files via upload

--- array.c
+++ array.c
@@ -1,12 +1,10 @@
-// find sum
+// find sum 
 #include<stdio.h>
-int main(int argc,char*argv[])
+int main(int argc, char* argv[])
 {
 	//local variable declaration
-	int sum = 0,a,b;
+	int sum = 0;
 	//code
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
 
 	sum = atoi(argv[1]) + atoi(argv[2]);
 
