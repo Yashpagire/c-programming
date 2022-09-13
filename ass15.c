bb38981078b1b7b3c24fd1073ed2ea4314fdf953
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-09-13 21:19:29 +0530

    Create ass15.c

---
+++ ass15.c
@@ -0,0 +1,22 @@
+//declare array and assign 5 char and print all char
+#include<stdio.h>
+int main()
+{
+	char arr[5];
+	
+
+	arr[0] = 'Y';
+	arr[1] = 'A';
+	arr[2] = 'S';
+	arr[3] = 'H';
+	arr[4] = 'P';
+
+	
+	printf("\narr[0]=%c", arr[0]);
+	printf("\narr[1]=%c", arr[1]);
+	printf("\narr[2]=%c", arr[2]);
+	printf("\narr[3]=%c", arr[3]);
+	printf("\narr[4]=%c", arr[4]);
+
+	return(0);
+}
