f2506db2a5807f553f603ffb87b013adb94a4869
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-09-13 21:08:45 +0530

    Create ass5.c

---
+++ ass5.c
@@ -0,0 +1,19 @@
+/*print bellow pattern  00 01 02 03
+                        10 11 12 13
+                        20 21 22 23
+                        30 31 32 33 */
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            printf("%d%d \t", i, j);
+        }
+        printf("\n");
+    }
+    
+}
