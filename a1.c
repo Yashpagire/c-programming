9ed600a7c32c0c0dbc0fa0e6fcfa635f10bc25bf
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-10-07 11:29:23 +0530

    Add files via upload

---
+++ a1.c
@@ -0,0 +1,14 @@
+//print value and address
+#include<stdio.h>
+int main()
+{
+	//local variable declaration
+	int n = 10;
+	//code
+	printf("value of n is %d", n);
+	printf("address of n is %p", &n);
+
+	return(0);
+
+
+}
