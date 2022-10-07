54e0bb52773d0c6cd8a92cdf1c75a97551791f09
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-10-07 11:48:12 +0530

    Add files via upload

---
+++ Typedef1.c
@@ -0,0 +1,15 @@
+//print string using typedef
+#include<stdio.h>
+
+typedef char* string;
+typedef char* string;
+
+int main()
+{
+	string str = "hello";
+	string str1 = "welcome";
+
+	printf("\n%s", str);
+	printf("\n%s", str1);
+	return(0);
+}
