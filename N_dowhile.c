ba6ebef4f7c984a4253a664f893e07849e656061
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-10-14 08:42:10 +0530

    Add files via upload

---
+++ N_dowhile.c
@@ -0,0 +1,22 @@
+/*print bellow pattern by using nested do while loop    *
+														* *
+														* * *
+														* * * *
+														* * * * *    */
+#include<stdio.h>
+ int main()
+{
+	int i = 1, j;
+	do
+	{
+		j = 1;
+		do
+		{
+			printf("*");
+			j++;
+		} while (j <= i);
+		i++;
+		printf("\n");
+	} while (i <= 5);
+	return(0);
+ }
