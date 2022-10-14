6e66db35b70d6772b6504e0d4b16820beb1b7c59
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-10-14 09:26:15 +0530

    Add files via upload

---
+++ pro_3.c
@@ -0,0 +1,16 @@
+//how the size of operator works
+#include<stdio.h>
+int main()
+{
+	//local variable
+	int a;
+	long int b;
+
+	printf("\nsize of a=%d", sizeof(a));
+	printf("\nsize of b=%d", sizeof(b));
+
+	printf("\nsize of int=%d", sizeof(int));
+
+	printf("\nsize of long =%d", sizeof(long));
+	
+}
