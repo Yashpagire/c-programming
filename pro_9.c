48311be8ab5589ea86c7f7604737361e05ffc628
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-10-14 09:31:12 +0530

    Add files via upload

---
+++ pro_9.c
@@ -0,0 +1,18 @@
+//write a program to decide whether a given year is leap or not
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("enter a year:");
+	scanf("%d", &year);
+
+	if (year % 100 != 0 && year % 4 == 0 || year % 400 == 0)
+	{
+		printf("\n%d is leap year", year);
+	}
+	else
+	{
+		printf("\n%d is not leap year", year);
+	}
+	return(0);
+}
