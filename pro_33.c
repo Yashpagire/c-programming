0f6a730f33e433b7dce2d757afebf197687e00e7
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-10-14 09:41:23 +0530

    Add files via upload

---
+++ pro_33.c
@@ -0,0 +1,10 @@
+/*Write a program to read and display a charecter using printfand scanf functions*/
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("\nenter a charechter:");
+	scanf("%c", &ch);
+
+	printf("\nYou enter:%c", ch);
+ }
