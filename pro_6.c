fd6ae1f7030033234bb9ef710ad635caa58ac952
Yash pagire <111268472+Yashpagire@users.noreply.github.com>
2022-10-14 09:30:07 +0530

    Add files via upload

---
+++ pro_6.c
@@ -0,0 +1,11 @@
+//dividing two integer numbers where type casting is done
+#include<stdio.h>
+int main()
+{
+	int x = 3, y = 2;
+	float z = (float)x / y;
+
+	printf("\nresult of division=%f", z);
+
+	return(0);
+}
